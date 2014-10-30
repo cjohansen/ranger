f34290a9d7f6232d493fbec5adc7870f7fef3118
Christian Johansen <n06974@nrkmci66780.felles.ds.nrk.no>
2014-10-30 12:00:55 +0100

    Up build

--- dist/ranger.js
+++ dist/ranger.js
@@ -4,8 +4,9 @@ var mmouse = require('mmouse');
 
 function defaultAction(fn) {
   return function (e) {
-    e.preventDefault();
-    fn(e);
+    if (!fn(e)) {
+      e.preventDefault();
+    }
   };
 }
 
